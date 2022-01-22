129e5c3b283b5db7059a171e46a6229c4518a1d4
dhouha <dhouha.mansour11@gmail.com>
2022-01-22 22:32:45 +0100

    update login unit test

--- tests/unit/Login.spec.js
+++ tests/unit/Login.spec.js
@@ -1,8 +1,8 @@
 import { shallowMount } from '@vue/test-utils'
 import Login from '@/components/Login.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+describe('Login.vue', () => {
+  it('renders login message when passed', () => {
     const msg = 'Log in to your Note Taken Application'
     const wrapper = shallowMount(Login, {
       propsData: { msg }
