0baf7e1be2575681691e825df872d57a16eb70cc
dhouha <dhouha.mansour11@gmail.com>
2022-01-23 13:11:33 +0100

    update add note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -7,23 +7,20 @@
     >
       Add
     </button>
-
     <div
-      v-for="note in notes"
-      v-bind:key="note.id"
       class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-5"
     >
-      <div id="newNote" style="display: none">
+      <div id="newnote" style="display: none">
         <div
           class="w-full h-64 flex flex-col justify-between bg-red-100 rounded-lg mb-6 py-5 px-4"
         >
-          <input
+          <textarea
             v-model="notetext"
             type="text"
             placeholder="Your new note"
             class="w-full h-64 flex flex-col justify-between bg-red-100 rounded-lg mb-6 py-5 px-4"
-          />
-          <!-- </input> -->
+          >
+          </textarea>
           <div class="flex justify-between">
             <button
               class="bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
@@ -32,16 +29,15 @@
             >
               Add
             </button>
-            <button
-              class="bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
-              type="button"
-              v-on:click="deleteNote(note.id)"
-            >
-              Delete
-            </button>
           </div>
         </div>
       </div>
+    </div>
+    <div
+      v-for="note in notes"
+      v-bind:key="note.id"
+      class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-5"
+    >
       <div
         class="w-full h-64 flex flex-col justify-between bg-red-100 rounded-lg mb-6 py-5 px-4"
       >
@@ -100,7 +96,7 @@ export default {
   },
   methods: {
     newNote() {
-      document.getElementById("newNote").style.display = "block";
+      document.getElementById("newnote").style.display = "block";
     },
     addNote() {
       const headers = {
@@ -111,27 +107,9 @@ export default {
           "https://beta.mailbutler.io/api/v2/notes",
           {
             context: {
-              message_id: "1",
-              gmail_message_id: "string",
-              gmail_draft_id: "string",
-              mailbutler_message_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              contact_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+              message_id: "1"
             },
-            text: this.notetext,
-            team_id: "Unknown Type: string,null",
-            meta: {
-              to: [
-                {
-                  contact_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                  name: "string",
-                },
-              ],
-              subject: "note title 3",
-              sender: {
-                contact_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                name: "string",
-              },
-            },
+            text: this.notetext
           },
           { headers }
         )
@@ -151,27 +129,9 @@ export default {
           `https://beta.mailbutler.io/api/v2/notes/` + noteId,
           {
             context: {
-              message_id: "1",
-              gmail_message_id: "string",
-              gmail_draft_id: "string",
-              mailbutler_message_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-              contact_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+              message_id: "1"
             },
-            text: noteText,
-            team_id: "Unknown Type: string,null",
-            meta: {
-              to: [
-                {
-                  contact_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                  name: "string",
-                },
-              ],
-              subject: "note title 3",
-              sender: {
-                contact_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                name: "string",
-              },
-            },
+            text: noteText
           },
           { headers }
         )
