8635fb4de84efb084068129d2386bb9d6d8ab1ed
dhouha <dhouha.mansour11@gmail.com>
2022-01-23 12:22:25 +0100

    add note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,66 +1,72 @@
 <template>
   <div class="container ml-5 mt-5">
     <button
-      class="bg-gray-800 mb-5 mr-5 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
+      class="bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
       type="button"
-      v-on:click="addNote"
+      v-on:click="newNote"
     >
-      Add Note
+      Add
     </button>
-    <div v-for="note in notes" v-bind:key="note.id">
-      <div>
-        <textarea v-model="note.text"
-        class="w-64 h-64 flex flex-col justify-between bg-red-100 rounded-lg mb-6 py-5 px-4"
-      >
-      </textarea>
-    <button
-      class="bg-gray-800 mb-5 mr-5 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
-      type="button"
-      v-on:click="SaveNote(note.id, note.text)"
+
+    <div
+      v-for="note in notes"
+      v-bind:key="note.id"
+      class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-5"
     >
-      Save Note
-    </button>
-    <button
-      class="bg-gray-800 mb-5 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
-      type="button"
-      v-on:click="deleteNote(note.id)"
-    >
-      Delete Note
-    </button>
-      </div>
-      <!-- <div
-        class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-5"
-      >
+      <div id="newNote" style="display: none">
         <div
           class="w-full h-64 flex flex-col justify-between bg-red-100 rounded-lg mb-6 py-5 px-4"
         >
-          <div>
-            <p class="text-gray-800 text-sm">
-              {{ note.text }}
-            </p>
-          </div>
-          <div class="flex items-center justify-between text-gray-800">
-            <p class="text-sm">{{ note.created_at }}</p>
+          <input
+            v-model="notetext"
+            type="text"
+            placeholder="Your new note"
+            class="w-full h-64 flex flex-col justify-between bg-red-100 rounded-lg mb-6 py-5 px-4"
+          />
+          <!-- </input> -->
+          <div class="flex justify-between">
             <button
-              class="w-8 h-8 rounded-full bg-gray-800 text-white flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-offset-2 ring-offset-pink-300 focus:ring-black"
-              aria-label="edit note"
-              role="button"
-              v-on:click="editNote"
+              class="bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
+              type="button"
+              v-on:click="addNote"
             >
-              <img
-                class="dark:hidden"
-                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/4-by-3-multiple-styled-cards-svg1.svg"
-                alt="edit"
-              />
-              <img
-                class="dark:block hidden"
-                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/4-by-3-multiple-styled-cards-svg1dark.svg"
-                alt="edit"
-              />
+              Add
+            </button>
+            <button
+              class="bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
+              type="button"
+              v-on:click="deleteNote(note.id)"
+            >
+              Delete
             </button>
           </div>
         </div>
-      </div> -->
+      </div>
+      <div
+        class="w-full h-64 flex flex-col justify-between bg-red-100 rounded-lg mb-6 py-5 px-4"
+      >
+        <textarea
+          v-model="note.text"
+          class="w-full h-64 flex flex-col justify-between bg-red-100 rounded-lg mb-6 py-5 px-4"
+        >
+        </textarea>
+        <div class="flex justify-between">
+          <button
+            class="bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
+            type="button"
+            v-on:click="SaveNote(note.id, note.text)"
+          >
+            Save
+          </button>
+          <button
+            class="bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10"
+            type="button"
+            v-on:click="deleteNote(note.id)"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -74,9 +80,7 @@ export default {
     return {
       notes: [],
       token: localStorage.getItem("token"),
-      title: "",
-      editing: "",
-      text: "",
+      notetext: "",
     };
   },
   mounted() {
@@ -95,6 +99,9 @@ export default {
       });
   },
   methods: {
+    newNote() {
+      document.getElementById("newNote").style.display = "block";
+    },
     addNote() {
       const headers = {
         Authorization: "Bearer " + this.token,
@@ -110,7 +117,7 @@ export default {
               mailbutler_message_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
               contact_id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
             },
-            text: "new note 5",
+            text: this.notetext,
             team_id: "Unknown Type: string,null",
             meta: {
               to: [
@@ -180,10 +187,9 @@ export default {
         Authorization: "Bearer " + this.token,
       };
       axios
-        .delete(
-          `https://beta.mailbutler.io/api/v2/notes/` + noteId,
-          { headers }
-        )
+        .delete(`https://beta.mailbutler.io/api/v2/notes/` + noteId, {
+          headers,
+        })
         .then((response) => {
           console.log(response);
         })
