059bc1df0a1bf342efbb80c505867765b31fb8bf
dhouha <dhouha.mansour11@gmail.com>
2022-01-24 09:27:24 +0100

    testing

--- tests/unit/Login.spec.js
+++ tests/unit/Login.spec.js
@@ -2,9 +2,9 @@ import { render, screen } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
 import Login from "@/components/Login.vue";
 import "@testing-library/jest-dom";
-import axios from 'axios'
+import axios from "axios";
 
-jest.spyOn(axios, 'post').mockResolvedValue(true);
+jest.spyOn(axios, "post").mockResolvedValue(true);
 
 describe("Login.vue", () => {
   it("should display login form and sign in button", () => {
@@ -22,13 +22,22 @@ describe("Login.vue", () => {
     ).toBeInTheDocument();
   });
 
-  it("should make API call when filling the form and click on Sign in button", async () => {
+  it("should make API call when filling the form and clicking on Sign in button", async () => {
     render(Login);
-    
-    await userEvent.type(screen.getByPlaceholderText("Your email address"), 'dhouha@dhouha.com')
-    await userEvent.type(screen.getByPlaceholderText("Your password"), 'test1234*')
+
+    await userEvent.type(
+      screen.getByPlaceholderText("Your email address"),
+      "dhouha@dhouha.com"
+    );
+    await userEvent.type(
+      screen.getByPlaceholderText("Your password"),
+      "test1234*"
+    );
     await userEvent.click(screen.getByRole("button", { name: /Sign in/i }));
 
-    expect(axios.post).toHaveBeenCalledWith('https://beta.mailbutler.io/api/v2/users/login', {email:'dhouha@dhouha.com', password:'test1234*'});
+    expect(axios.post).toHaveBeenCalledWith(
+      "https://beta.mailbutler.io/api/v2/users/login",
+      { email: "dhouha@dhouha.com", password: "test1234*" }
+    );
   });
 });
--- tests/unit/Note.spec.js
+++ tests/unit/Note.spec.js
@@ -2,20 +2,25 @@ import { render, screen } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
 import Note from "@/components/Note.vue";
 import "@testing-library/jest-dom";
+import axios from 'axios'
+
+jest.spyOn(axios, 'post').mockResolvedValue(true);
+jest.spyOn(axios, 'put').mockResolvedValue(true);
 
 describe("Note.vue", () => {
   it("should display new note template", () => {
     render(Note);
+
     expect(screen.getByPlaceholderText("Your new note")).toBeInTheDocument();
     expect(screen.getByTitle("Add")).toBeInTheDocument();
   });
 
-  it("should add new note after clicking on add button", async () => {
+  it("should make API call after typing a text and clicking on add button", async () => {
     render(Note);
-    userEvent.type(screen.getByRole("textbox", {name: ""}), "test note");
-    userEvent.click(screen.getByRole("button", {name: "Add"}));
-    // expect(await screen.findByText('test note')).toBeInTheDocument();
-    // expect(await screen.findByTitle("Save")).toBeInTheDocument();
-    // expect(await screen.findByTitle("Delete")).toBeInTheDocument();
+
+    await userEvent.type(screen.getByRole("textbox", {name: ""}), "test note");
+    await userEvent.click(screen.getByRole("button", {name: "Add"}));
+
+    expect(axios.post).toHaveBeenCalledTimes(1);
   });
 });
