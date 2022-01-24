a95f14f3bbb9dc5d6b864fc15168e5fc8db0457d
dhouha <dhouha.mansour11@gmail.com>
2022-01-24 08:50:04 +0100

    update login test

--- tests/unit/Login.spec.js
+++ tests/unit/Login.spec.js
@@ -9,6 +9,7 @@ jest.spyOn(axios, 'post').mockResolvedValue(true);
 describe("Login.vue", () => {
   it("should display login form and sign in button", () => {
     render(Login);
+
     expect(
       screen.getByText("Login To Your Notes Application")
     ).toBeInTheDocument();
@@ -21,10 +22,13 @@ describe("Login.vue", () => {
     ).toBeInTheDocument();
   });
 
-  it("should make API call when click on Sign in button", async () => {
+  it("should make API call when filling the form and click on Sign in button", async () => {
     render(Login);
     
+    await userEvent.type(screen.getByPlaceholderText("Your email address"), 'dhouha@dhouha.com')
+    await userEvent.type(screen.getByPlaceholderText("Your password"), 'test1234*')
     await userEvent.click(screen.getByRole("button", { name: /Sign in/i }));
-    expect(axios.post).toHaveBeenCalled()
+
+    expect(axios.post).toHaveBeenCalledWith('https://beta.mailbutler.io/api/v2/users/login', {email:'dhouha@dhouha.com', password:'test1234*'});
   });
 });
